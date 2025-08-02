c9dbf617740b75e5e95e15783c97ca0afdd086ae
zahidkhancodes <zahidkh323@gmail.com>
2025-08-02 19:20:19 +0530

    Create pointer_array_demo.cpp

---
+++ 2025_08_02/pointer_array_demo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int arr[10] = {23, 122, 41, 67};
+
+    cout << "address of first memory block is " << arr << endl;
+    cout << arr[0] << endl;
+    cout << "address of first memory block is " << &arr[0] << endl;
+
+    cout << "4th " << *arr << endl;
+    cout << "5th " << *arr + 1 << endl;
+    cout << "6th " << *(arr + 1) << endl;
+    cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+    cout << "9th " << *(arr+2) << endl;
+
+    int i = 3;
+    cout << i[arr] << endl;
+
+    int temp[10] = {1, 2};
+    cout << sizeof(temp) << endl;
+    cout << "1st " << sizeof(*temp) << endl;
+    cout << "2nd " << sizeof(&temp) << endl;
+
+    int *ptr = &temp[0];
+    cout << sizeof(ptr) << endl;
+    cout << sizeof(*ptr) << endl;
+    cout << sizeof(&ptr) << endl;
+
+    return 0;
+}
