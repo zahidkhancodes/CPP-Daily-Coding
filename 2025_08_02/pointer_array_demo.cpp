ab2bd46139f2a258f1b50e684791c51b1b173e1d
zahidkhancodes <zahidkh323@gmail.com>
2025-08-02 19:45:39 +0530

    Update pointer_array_demo.cpp

--- 2025_08_02/pointer_array_demo.cpp
+++ 2025_08_02/pointer_array_demo.cpp
@@ -2,32 +2,31 @@
 using namespace std;
 
 int main () {
-
     int arr[10] = {23, 122, 41, 67};
 
-    cout << "address of first memory block is " << arr << endl;
-    cout << arr[0] << endl;
-    cout << "address of first memory block is " << &arr[0] << endl;
+    cout << "Address of first element (arr) is: " << arr << endl;
+    cout << "Value at arr[0]: " << arr[0] << endl;
+    cout << "Address of first element (&arr[0]) is: " << &arr[0] << endl;
 
-    cout << "4th " << *arr << endl;
-    cout << "5th " << *arr + 1 << endl;
-    cout << "6th " << *(arr + 1) << endl;
-    cout << "7th " << *(arr) + 1 << endl;
-    cout << "8th " << arr[2] << endl;
-    cout << "9th " << *(arr+2) << endl;
+    cout << "Value pointed by arr (*arr): " << *arr << endl;
+    cout << "Value (*arr + 1): " << *arr + 1 << endl;
+    cout << "Value pointed by (arr + 1) (*(arr + 1)): " << *(arr + 1) << endl;
+    cout << "Value (*(arr) + 1): " << *(arr) + 1 << endl;
+    cout << "Value at arr[2]: " << arr[2] << endl;
+    cout << "Value (*(arr+2)): " << *(arr + 2) << endl;
 
     int i = 3;
-    cout << i[arr] << endl;
+    cout << "Value of i[arr] (i[arr] is valid and equals arr[i]): " << i[arr] << endl;
 
     int temp[10] = {1, 2};
-    cout << sizeof(temp) << endl;
-    cout << "1st " << sizeof(*temp) << endl;
-    cout << "2nd " << sizeof(&temp) << endl;
+    cout << "Size of temp array (sizeof(temp)): " << sizeof(temp) << " bytes" << endl;
+    cout << "Size of one element (*temp): " << sizeof(*temp) << " bytes" << endl;
+    cout << "Size of pointer to temp (&temp): " << sizeof(&temp) << " bytes" << endl;
 
     int *ptr = &temp[0];
-    cout << sizeof(ptr) << endl;
-    cout << sizeof(*ptr) << endl;
-    cout << sizeof(&ptr) << endl;
+    cout << "Size of pointer ptr: " << sizeof(ptr) << " bytes" << endl;
+    cout << "Size of element pointed by ptr (*ptr): " << sizeof(*ptr) << " bytes" << endl;
+    cout << "Size of pointer to ptr (&ptr): " << sizeof(&ptr) << " bytes" << endl;
 
     return 0;
 }
