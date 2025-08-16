7a58c88865fffcb77599a00440b8f7e20e719654
zahidkhancodes <zahidkh323@gmail.com>
2025-08-16 23:20:53 +0530

    Create static_dynamic_class.cpp

---
+++ 2025_08_16/static_dynamic_class.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class Hero {
+    //properties
+    private:
+    int health;
+
+    public:
+    char level;
+
+    void print(){
+        cout << level << endl;
+}
+
+    int getHealth() {
+        return health;
+}
+
+    char getLevel() {
+        return level;
+}
+
+    void setHealth(int h) {
+        health = h;
+}
+
+    void setLevel(char ch) {
+        level = ch;
+}
+
+};
+
+int main() {
+    
+//static allocation
+Hero a;
+a.setHealth(80);
+a.setLevel('B');
+cout << "level is " << a.level << endl;
+cout << " health is " << a.getHealth() << endl;
+
+//dynamically
+Hero *b = new Hero;
+b->setLevel('A');
+b->setHealth(70);
+cout << "level is " << (*b).level << endl;
+cout << " health is " << (*b).getHealth() << endl;
+
+cout << "level is " << b->level << endl;
+cout << " health is " << b->getHealth() << endl;
+
+return 0;
+}
