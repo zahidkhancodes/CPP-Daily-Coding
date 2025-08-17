d3804f692fecad7638b5a479bd0c93e599c3e5f8
zahidkhancodes <zahidkh323@gmail.com>
2025-08-17 22:07:13 +0530

    Create Hero_copyconstructor.cpp

---
+++ 2025_08_17/Hero_copyconstructor.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Hero {
+    private:
+    int health;
+    public:
+    char* name;
+    char level;
+    
+    Hero(){
+        cout<<"Simple comstructor called " <<endl;
+        name = new char[100];
+    }
+    
+    // Copy Constructor
+    // Hero(Hero& temp) {
+    //     cout << "Copy constructor called" << endl;
+    //     this->health = temp.health;
+    //     this->level = temp.level;
+    // }
+    
+    void print() {
+        cout << endl;
+        cout << "[ Name: " << this->name<< " , ";
+        cout << "health: " << this->health << " , ";
+        cout << "level: " << this->level << " . ]";
+        cout << endl<< endl;
+    }
+    
+    int getHealth() {
+        return health;
+    }
+    
+    char getLevel() {
+        return level;
+    }
+    
+    void setHealth(int h) {
+        health = h;
+    }
+
+    void setLevel(char ch) {
+        level = ch;
+    }
+    
+    void setName (char name[]) {
+        strcpy(this->name , name);
+    }
+};
+
+int main() {
+
+    Hero hero1;
+
+    hero1.setHealth(12);
+    hero1.setLevel('D');
+    char name[7] = "Babbar";
+    hero1.setName(name);
+
+    hero1.print();
+
+    return 0;
+}
