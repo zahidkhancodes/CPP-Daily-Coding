a71fdb6870259b8c5a9788e985ebe28d8e14cdf6
zahidkhancodes <zahidkh323@gmail.com>
2025-08-17 21:49:56 +0530

    Update copy_constructor.cpp

--- 2025_08_17/copy_constructor.cpp
+++ 2025_08_17/copy_constructor.cpp
@@ -1,1 +1,54 @@
+#include<iostream>
+using namespace std;
 
+class Hero {
+    private:
+    int health;
+    public:
+    char level;
+    
+    // Parameterized constructor
+    Hero(int h, char l) {
+        health = h;
+        level = l;
+    }
+    
+    // Copy Constructor
+    Hero(Hero& temp) {
+        cout << "Copy constructor called" << endl;
+        this->health = temp.health;
+        this->level = temp.level;
+    }
+    
+    void print(){
+        cout << "Health: " << health << ", Level: " << level << endl;
+    }
+    
+    int getHealth() {
+        return health;
+    }
+    
+    char getLevel() {
+        return level;
+    }
+    
+    void setHealth(int h) {
+        health = h;
+    }
+
+    void setLevel(char ch) {
+        level = ch;
+    }
+};
+
+int main() {
+    Hero S(70 , 'C');
+    S.print();
+    
+    //Copy constructor
+    Hero R(S);
+    R.print();
+
+    
+    return 0;
+}
