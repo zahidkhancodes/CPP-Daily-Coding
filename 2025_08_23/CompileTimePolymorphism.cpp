8c17c20b97a8814a69f3844f17a463c5056aca8b
zahidkhancodes <zahidkh323@gmail.com>
2025-08-23 21:51:21 +0530

    Create CompileTimePolymorphism.cpp

---
+++ 2025_08_23/CompileTimePolymorphism.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class A{
+    public:
+    void func(){
+        cout << "Hello " << endl;
+    }
+    
+    void func(string name){
+        cout << "Hii " << name << endl;
+    }
+    
+    int func (char name){
+        cout<<"Hlw "<<name<< endl;
+        return 1 ;
+    }
+};
+
+int main(){
+    A Greeting;
+    Greeting.func();
+    Greeting.func("Babbar");
+    Greeting.func('H');
+    
+    return 0;
+}
