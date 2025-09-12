fbb9ceea73213c01750e5aefb941366ba9dbb4cf
zahidkhancodes <zahidkh323@gmail.com>
2025-09-12 22:19:21 +0530

    Create Wasted.cpp

---
+++ 2025_09_12/Wasted.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+    void print1();
+    
+    int main(){
+        print1();
+        return 0;
+    }
+    
+    void print1(){
+        cout<<"PPT is Ongoing";
+    }
